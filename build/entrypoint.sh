aa521b23c2b1f418f15c326b9d81e8c627bb50b6
Joey Patino <joseph.onitap@gmail.com>
2025-02-21 00:46:48 +0100

    add TLS support to entry point

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -19,22 +19,21 @@ if [[ $1 == init ]]; then
     /protonmail/proton-bridge --cli $@
 
 else
-
-    # socat will make the conn appear to come from 127.0.0.1
-    # ProtonMail Bridge currently expects that.
-    # It also allows us to bind to the real ports :)
-    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
-
     # If the TLS_CERT_PATH file exists, also listen on TLS ports
-    if [[ -f "$TLS_CERT_PATH" ]]; then
-        # SMTP over TLS on port 587
-        socat OPENSSL-LISTEN:587,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1587 &
-        # IMAP over TLS on port 993
-        socat OPENSSL-LISTEN:993,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1993 &
-    else
-        echo "TLS_CERT_PATH is set to '$TLS_CERT_PATH', but the file was not found. TLS ports will not be opened."
-    fi
+#    if [[ -f "$TLS_CERT_PATH" ]]; then
+#        # SMTP over TLS on port 25
+#        socat OPENSSL-LISTEN:25,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1025 &
+#        # IMAP over TLS on port 143
+#        socat OPENSSL-LISTEN:143,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1143 &
+#    else
+#        echo "TLS_CERT_PATH is set to '$TLS_CERT_PATH', but the file was not found. TLS ports will not be opened."
+
+        # socat will make the conn appear to come from 127.0.0.1
+        # ProtonMail Bridge currently expects that.
+        # It also allows us to bind to the real ports :)
+        socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+        socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+#    fi
 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
--- deb/entrypoint.sh
+++ deb/entrypoint.sh
@@ -5,26 +5,6 @@ set -ex
 # Initialize
 if [[ $1 == init ]]; then
 
-    # # Parse parameters
-    # TFP=""  # Default empty two factor passcode
-    # shift  # skip `init`
-    # while [[ $# -gt 0 ]]; do
-    #     key="$1"
-    #     case $key in
-    #         -u|--username)
-    #         USERNAME="$2"
-    #         ;;
-    #         -p|--password)
-    #         PASSWORD="$2"
-    #         ;;
-    #         -t|--twofactor)
-    #         TWOFACTOR="$2"
-    #         ;;
-    #     esac
-    #     shift
-    #     shift
-    # done
-
     # Initialize pass
     gpg --generate-key --batch /protonmail/gpgparams
     pass init pass-key
@@ -34,11 +14,21 @@ if [[ $1 == init ]]; then
 
 else
 
-    # socat will make the conn appear to come from 127.0.0.1
-    # ProtonMail Bridge currently expects that.
-    # It also allows us to bind to the real ports :)
-    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    # If the TLS_CERT_PATH file exists, also listen on TLS ports
+#    if [[ -f "$TLS_CERT_PATH" ]]; then
+#        # SMTP over TLS on port 25
+#        socat OPENSSL-LISTEN:25,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1025 &
+#        # IMAP over TLS on port 143
+#        socat OPENSSL-LISTEN:143,cert="$TLS_CERT_PATH",verify=0,fork TCP:127.0.0.1:1143 &
+#    else
+#        echo "TLS_CERT_PATH is set to '$TLS_CERT_PATH', but the file was not found. TLS ports will not be opened."
+
+        # socat will make the conn appear to come from 127.0.0.1
+        # ProtonMail Bridge currently expects that.
+        # It also allows us to bind to the real ports :)
+        socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+        socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+#    fi
 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
